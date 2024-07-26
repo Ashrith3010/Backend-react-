7cea11afcb63108f0aab51df8e75f0712995696f
Ashrith3010 <ashrith6974@gmail.com>
2024-07-26 18:37:58 +0530

    Backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,17 +1,20 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
+ 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Extract token from Bearer schema
-
-  if (token == null) return res.sendStatus(401); // If no token, unauthorized
-
+  const token = authHeader && authHeader.split(' ')[1];
+ 
+  if (token == null) return res.sendStatus(401);
+ 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // If token invalid, forbidden
-    req.user = user; // Store user info in request
+    if (err) return res.sendStatus(403);
+ 
+    req.user = { id: user.id };
     next();
   });
 };
-
+ 
 module.exports = authenticateToken;
+ 
+ 
